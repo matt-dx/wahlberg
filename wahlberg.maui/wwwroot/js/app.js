f988d479bda50cdde75dc0fc063b1e4006bfa77d
Matthew Whitwam <Matthew.Whitwam@structureit.net>
2026-04-22 20:36:31 +0200

    Enhance link handling in the app by adding support for opening external URLs and relative links

--- wahlberg.maui/wwwroot/js/app.js
+++ wahlberg.maui/wwwroot/js/app.js
@@ -1,5 +1,6 @@
 window.appInterop = {
     _scrollHandler: null,
+    _linkClickHandler: null,
     _dotNetRef: null,
     _dropDotNetRef: null,
     _lastActiveId: '',
@@ -57,6 +58,7 @@ window.appInterop = {
         });
 
         this._setupScrollTracking();
+        this._setupLinkHandling();
         await this._renderMermaid();
     },
 
@@ -65,6 +67,35 @@ window.appInterop = {
         if (el) {
             el.scrollIntoView({ behavior: 'smooth', block: 'start' });
         }
+    },
+
+    _setupLinkHandling: function () {
+        const container = document.querySelector('.document-content');
+        if (!container) return;
+
+        if (this._linkClickHandler) {
+            container.removeEventListener('click', this._linkClickHandler);
+        }
+
+        const self = this;
+        this._linkClickHandler = function (e) {
+            const anchor = e.target.closest('a[href]');
+            if (!anchor) return;
+
+            const href = anchor.getAttribute('href');
+            if (!href || href.startsWith('#') || href.startsWith('javascript:')) return;
+
+            e.preventDefault();
+            if (!self._dotNetRef) return;
+
+            if (/^https?:\/\//i.test(href) || /^mailto:/i.test(href)) {
+                self._dotNetRef.invokeMethodAsync('OpenExternalUrl', href);
+            } else {
+                self._dotNetRef.invokeMethodAsync('OpenRelativeLink', href);
+            }
+        };
+
+        container.addEventListener('click', this._linkClickHandler);
     },
 
     _setupScrollTracking: function () {
@@ -133,10 +164,12 @@ window.appInterop = {
 
     dispose: function () {
         const container = document.querySelector('.document-content');
-        if (container && this._scrollHandler) {
-            container.removeEventListener('scroll', this._scrollHandler);
+        if (container) {
+            if (this._scrollHandler) container.removeEventListener('scroll', this._scrollHandler);
+            if (this._linkClickHandler) container.removeEventListener('click', this._linkClickHandler);
         }
         this._scrollHandler = null;
+        this._linkClickHandler = null;
         this._dotNetRef = null;
         this._dropDotNetRef = null;
         this._lastActiveId = '';
